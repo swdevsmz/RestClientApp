c1d3feaa6c7d809ef7e6ca91f509cc75c28e0f79
swdev.smz@gmail.com <swdev.smz@gmail.com>
2019-08-28 23:17:42 +0900

    Regex.Unescapeを追加

--- RestClientApp/RestClientApp/Form1.Designer.cs
+++ RestClientApp/RestClientApp/Form1.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(349, 262);
+            this.button1.Location = new System.Drawing.Point(713, 13);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -44,21 +45,31 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Location = new System.Drawing.Point(4, 42);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(784, 243);
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(784, 329);
             this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(669, 19);
+            this.textBox2.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -68,6 +79,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- RestClientApp/RestClientApp/Form1.cs
+++ RestClientApp/RestClientApp/Form1.cs
@@ -6,7 +6,9 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using RestSharp;
@@ -15,47 +17,51 @@ namespace RestClientApp
 {
     public partial class Form1 : Form
     {
+
+        private static HttpClient client = new HttpClient();
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Button1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-
-            // クライアント＆リクエストの作成
-            var client = new RestClient();
-            var request = new RestRequest();
-
-            // URLの設定
-            //client.BaseUrl = new Uri("http://localhost:8080/json");
-            client.BaseUrl = new Uri("http://weather.livedoor.com/forecast/webservice/json/v1?city=400040");
-            // メソッド、パラメータの指定
-            request.Method = Method.POST;
-            //request.AddParameter("パラメータ名", "パラメータの値", ParameterType.GetOrPost);
-            request.AddParameter("application/json", "body", ParameterType.RequestBody);
-            // Authorization: Basic Zm9vOmJhcg==
-            // ParameterTypeはいろいろあるが、GETとPOSTで特に指定なく
-            // stringパラメータを設定する場合は、GetOrPost
-
-            // ファイルをアップロードする場合
-            //request.AddFile("ファイルパラメータ名", "ファイルパス", "ContentType");
-            // ContentTypeは拡張子などから適切なものを選ぶ
-
-            // リクエスト送信
-            var response = client.Execute(request);
-
-            // レスポンスがファイルなどで、復元したい場合
-            //File.WriteAllBytes("出力先のパス", response.RawBytes);
-
-            // レスポンスのステータスコードが欲しいなどの場合
-            bool isOK = response.StatusCode == HttpStatusCode.OK;
-            // ステータスコード以外にも様々な情報がresponseに入っているので適宜
-
-            //Console.WriteLine(response);
-
-            textBox1.Text = response.Content;
-
+            textBox2.Text = "http://weather.livedoor.com/forecast/webservice/json/v1?city=400040";
         }
+
+
+        private async void Button1_Click(object sender, EventArgs e)
+        {
+            //var response = await client.GetAsync(textBox2.Text);
+            
+            var request = new HttpRequestMessage(HttpMethod.Get, textBox2.Text);
+            request.Headers.Add("ContentType", "application/json; charset=utf-8");
+            //request.Headers.Add("Authorization", "Bearer " + "");
+            //request.Headers.Add("Content-type", "text/html; charset=UTF-8");
+            var response = await client.SendAsync(request);
+            //Console.WriteLine(response.StatusCode);
+            //var content = await response.Content.ReadAsByteArrayAsync();
+            //var encoding = Encoding.ASCII;
+            //this.textBox1.Text = encoding.GetString(content);
+            //var content = await response.Content.ReadAsStringAsync();
+            //string s = await UTF8ResponseMessageAsync(response);
+
+            //var str = System.Text.Encoding.UTF8.GetString(await response.Content.ReadAsStringAsync());
+
+            this.textBox1.Text = Regex.Unescape(response.Content.ReadAsStringAsync().Result);
+
+            //this.textBox1.Text = str;
+        }
+
+        private async Task<string> UTF8ResponseMessageAsync(HttpResponseMessage result)
+        {
+            //str = System.Text.Encoding.UTF8.GetString(bytesData);
+
+            byte[] isoBites = Encoding.GetEncoding("ISO-8859-1").GetBytes(Regex.Unescape(await result.Content.ReadAsStringAsync()));
+            return Encoding.UTF8.GetString(isoBites, 0, isoBites.Length);
+        }
+
+
     }
 }
