674939e4702e7a60caeb96b7d64f30e01274bbd4
swdev.smz@gmail.com <swdev.smz@gmail.com>
2019-09-12 22:48:04 +0900

    可変のテキストボックスを追加

--- RestClientApp/RestClientApp/Form1.Designer.cs
+++ RestClientApp/RestClientApp/Form1.Designer.cs
@@ -32,12 +32,15 @@
             this.Result = new System.Windows.Forms.TextBox();
             this.Url = new System.Windows.Forms.TextBox();
             this.HttpStatus = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Minus = new System.Windows.Forms.Button();
+            this.Plus = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // requestSend
             // 
             this.requestSend.Location = new System.Drawing.Point(951, 16);
-            this.requestSend.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.requestSend.Margin = new System.Windows.Forms.Padding(4);
             this.requestSend.Name = "requestSend";
             this.requestSend.Size = new System.Drawing.Size(100, 29);
             this.requestSend.TabIndex = 0;
@@ -47,40 +50,71 @@
             // 
             // Result
             // 
-            this.Result.Location = new System.Drawing.Point(17, 123);
-            this.Result.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Result.Location = new System.Drawing.Point(17, 275);
+            this.Result.Margin = new System.Windows.Forms.Padding(4);
             this.Result.Multiline = true;
             this.Result.Name = "Result";
             this.Result.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.Result.Size = new System.Drawing.Size(1044, 410);
+            this.Result.Size = new System.Drawing.Size(1034, 410);
             this.Result.TabIndex = 1;
             // 
             // Url
             // 
             this.Url.Location = new System.Drawing.Point(17, 16);
-            this.Url.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Url.Margin = new System.Windows.Forms.Padding(4);
             this.Url.Name = "Url";
             this.Url.Size = new System.Drawing.Size(891, 22);
             this.Url.TabIndex = 2;
             // 
             // HttpStatus
             // 
-            this.HttpStatus.Location = new System.Drawing.Point(17, 93);
+            this.HttpStatus.Location = new System.Drawing.Point(17, 245);
             this.HttpStatus.Margin = new System.Windows.Forms.Padding(4);
             this.HttpStatus.Name = "HttpStatus";
-            this.HttpStatus.Size = new System.Drawing.Size(891, 22);
+            this.HttpStatus.Size = new System.Drawing.Size(1037, 22);
             this.HttpStatus.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(17, 95);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1038, 143);
+            this.panel1.TabIndex = 4;
+            // 
+            // Minus
+            // 
+            this.Minus.Location = new System.Drawing.Point(123, 66);
+            this.Minus.Name = "Minus";
+            this.Minus.Size = new System.Drawing.Size(75, 23);
+            this.Minus.TabIndex = 1;
+            this.Minus.Text = "-";
+            this.Minus.UseVisualStyleBackColor = true;
+            this.Minus.Click += new System.EventHandler(this.Minus_Click);
+            // 
+            // Plus
+            // 
+            this.Plus.Location = new System.Drawing.Point(34, 66);
+            this.Plus.Name = "Plus";
+            this.Plus.Size = new System.Drawing.Size(75, 23);
+            this.Plus.TabIndex = 0;
+            this.Plus.Text = "+";
+            this.Plus.UseVisualStyleBackColor = true;
+            this.Plus.Click += new System.EventHandler(this.Plus_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 562);
+            this.ClientSize = new System.Drawing.Size(1067, 698);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.HttpStatus);
+            this.Controls.Add(this.Plus);
+            this.Controls.Add(this.Minus);
             this.Controls.Add(this.Url);
             this.Controls.Add(this.Result);
             this.Controls.Add(this.requestSend);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -95,6 +129,9 @@
         private System.Windows.Forms.TextBox Result;
         private System.Windows.Forms.TextBox Url;
         private System.Windows.Forms.TextBox HttpStatus;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Minus;
+        private System.Windows.Forms.Button Plus;
     }
 }
 
--- RestClientApp/RestClientApp/Form1.cs
+++ RestClientApp/RestClientApp/Form1.cs
@@ -30,22 +30,22 @@ namespace RestClientApp
             Url.Text = "http://weather.livedoor.com/forecast/webservice/json/v1?city=400040";
         }
 
-
         private async void Send_Click(object sender, EventArgs e)
         {          
-            var request = new HttpRequestMessage(HttpMethod.Post, Url.Text);
-            
-            // リクエストヘッダー情報の設定
-            // Content-typeはContentの第3引数で設定
-            //request.Headers.Add("ContentType", "application/json; charset=utf-8");
-            //request.Headers.Add("Content-Type", "MIME-Type-Here");
-            request.Headers.Add("Authorization", "Bearer " + "12345678");
-            request.Headers.Add("User-Agent", "User-Agent-Here");
-            request.Headers.Add("AAAA", "User-Agent-Here");
+            var request = new HttpRequestMessage(HttpMethod.Get, Url.Text);
 
-            // リクエストボディ情報の設定
-            var json = @"{""foo"":""hoge"", ""bar"":123, ""baz"":[""あ"", ""い"", ""う""]}";
-            request.Content = new StringContent(json, Encoding.UTF8, @"application/json");
+            for(int i = 1; i <= panel1.Controls.Count / 2; i++)
+            {
+                MessageBox.Show(panel1.Controls.Find($"Key{i}", false)[0].Text + panel1.Controls.Find($"Value{i}", false)[0].Text);
+            }
+
+            // リクエストヘッダの設定
+            //request.Headers.Add("Authorization", "Bearer " + "12345678");
+            //request.Headers.Add("User-Agent", "User-Agent-Here");
+            //request.Headers.Add("AAAA", "User-Agent-Here");
+            // リクエストボディの設定
+            //var json = @"{""foo"":""hoge"", ""bar"":123, ""baz"":[""あ"", ""い"", ""う""]}";
+            //request.Content = new StringContent(json, Encoding.UTF8, @"application/json");
 
             // 送信
             var response = await client.SendAsync(request);
@@ -53,9 +53,31 @@ namespace RestClientApp
             // 結果の表示
             this.HttpStatus.Text = ((int)response.StatusCode).ToString();
             this.Result.Text = Regex.Unescape(response.Content.ReadAsStringAsync().Result);
-
         }
 
+        private void Plus_Click(object sender, EventArgs e)
+        {
+            TextBox key   = new TextBox();
+            TextBox value = new TextBox();
 
+            key.Width = value.Width = 180;
+            key.Height = value.Height = 24;
+            key.Top = value.Top = 10 + (15 * panel1.Controls.Count);
+            key.Left = 10;
+            value.Left = key.Left + key.Width + 10;
+            key.Name = "Key" + (panel1.Controls.Count / 2 + 1);
+            value.Name = "Value" + (panel1.Controls.Count / 2 + 1);
+
+            this.panel1.Controls.Add(key);
+            this.panel1.Controls.Add(value);
+        }
+
+        private void Minus_Click(object sender, EventArgs e)
+        {
+            if (this.panel1.Controls.Count == 0) return;
+
+            this.panel1.Controls.RemoveAt(panel1.Controls.Count - 1);
+            this.panel1.Controls.RemoveAt(panel1.Controls.Count - 1);
+        }
     }
 }
